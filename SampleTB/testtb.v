0900634ae374c6850fe9a273d85df5f3f927ec78
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-07-09 10:25:55 +0530

    Update testtb.sv

--- SampleTB/testtb.v
+++ SampleTB/testtb.v
@@ -1,19 +1,18 @@
+//Test class
 class test extends uvm_test;
   `uvm_component_utils(test)
   function new(string name = "test", uvm_component parent=null);
     super.new(name, parent);
   endfunction
-  
   env e0;
-  virtual reg_if vif;
-  
+  virtual switch_if vif;
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     e0 = env::type_id::create("e0", this);
-    if (!uvm_config_db#(virtual reg_if)::get(this, "", "reg_vif", vif))
+    if (!uvm_config_db#(virtual switch_if)::get(this, "", "switch_vif", vif))
       `uvm_fatal("TEST", "Did not get vif")
       
-      uvm_config_db#(virtual reg_if)::set(this, "e0.a0.*", "reg_vif", vif);
+      uvm_config_db#(virtual switch_if)::set(this, "e0.a0.*", "switch_vif", vif);
   endfunction
   
   virtual task run_phase(uvm_phase phase);
@@ -21,9 +20,8 @@ class test extends uvm_test;
     phase.raise_objection(this);
     apply_reset();
     
-    seq.randomize() with {num inside {[20:30]}; };
+    seq.randomize();
     seq.start(e0.a0.s0);
-    #200;
     phase.drop_objection(this);
   endtask
   
